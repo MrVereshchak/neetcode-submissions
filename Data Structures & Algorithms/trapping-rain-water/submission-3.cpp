1fac1e8d483998e333e27d727924e1c90a762e77
Darii Vereshchak <55427187+MrVereshchak@users.noreply.github.com>
2026-07-16 17:07:38 -0700

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(const vector<int> &height) {
+        size_t l = 0;
+        size_t r = height.size() - 1;
+        int maxL = height[l];
+        int maxR = height[r];
+        int totalWaterVolume = 0;
+
+        while (l < r) {
+            if (maxR < maxL) {
+                --r;
+                maxR = max(maxR, height[r]);
+                totalWaterVolume += max(maxR - height[r], 0);
+            } else {
+                ++l;
+                maxL = max(maxL, height[l]);
+                totalWaterVolume += max(maxL - height[l], 0);
+            }
+        }
+
+        return totalWaterVolume;
+    }
+};
