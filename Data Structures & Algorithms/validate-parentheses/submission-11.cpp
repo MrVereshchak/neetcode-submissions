65df4764ef5e6c7ef1c24a738dffadc4a2737dfe
Darii Vereshchak <55427187+MrVereshchak@users.noreply.github.com>
2026-07-23 15:23:52 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(const string &s) {
+        if (s.size() % 2 != 0) {
+            return false;
+        }
+
+        stack<char> openPars;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                openPars.push(ch);
+            } else {
+                if (openPars.empty()) {
+                    return false;
+                }
+
+                char closedPar = ch;
+                char openPar = openPars.top();
+                openPars.pop();
+
+                if (!(openPar == '(' && closedPar == ')') &&
+                    !(openPar == '{' && closedPar == '}') &&
+                    !(openPar == '[' && closedPar == ']')) {
+                        return false;
+                    }
+            }
+        }
+
+        return openPars.empty();
+    }
+};
